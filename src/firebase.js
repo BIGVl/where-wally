53c73bc3680bf3650bd2c16125be70fd1587893f
bigVL <84589629+BIGVl@users.noreply.github.com>
2022-05-11 06:58:58 +0300

    Initial ccommit

---
+++ src/firebase.js
@@ -0,0 +1,16 @@
+import { initializeApp } from 'firebase/app';
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: 'AIzaSyCPsIrvQzyfPjqZscFgI35ivxl7Ee2Yn50',
+  authDomain: 'wally-not-found.firebaseapp.com',
+  projectId: 'wally-not-found',
+  storageBucket: 'wally-not-found.appspot.com',
+  messagingSenderId: '643983018940',
+  appId: '1:643983018940:web:46f1bfba518c184d1739b9'
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
